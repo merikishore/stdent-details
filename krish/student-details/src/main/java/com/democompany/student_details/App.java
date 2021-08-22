86f7ef000ed08ca4218740e349616766f93caa4e
merikishore <kishireecepg@gmail.com>
2021-08-22 22:18:51 +0530

    added initiat commit

---
+++ krish/student-details/src/main/java/com/democompany/student_details/App.java
@@ -0,0 +1,8 @@
+package com.democompany.student_details;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
